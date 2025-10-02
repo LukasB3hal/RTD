59c50e2f69ab63fc6a69f60eeb388ec79e8e5fe6
Major_Majsner22 <behall.08@spst.eu>
2025-10-02 12:29:51 +0200

    index.html + style.css + obrázek s Majsnerem
    Přidán index.html propojený se style.css.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+
+<body>
+<div class="stred" style="color:white;">
+<h1>Road to Dubai</h1>
+</div>
+
+
+</body>
+
+
+
+
+
+</html>
